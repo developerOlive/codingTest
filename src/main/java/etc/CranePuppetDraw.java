6d8a0a5729377bcf2b6394b9db204fe043f96f0d
developerOlive <chloehiho@gmail.com>
2022-07-26 16:38:27 +0900

    [수정] 문자 정렬 수정

--- src/main/java/etc/CranePuppetDraw.java
+++ src/main/java/etc/CranePuppetDraw.java
@@ -23,7 +23,11 @@ public class CranePuppetDraw {
 
     public static void main(String[] args) {
         CranePuppetDraw sol = new CranePuppetDraw();
-        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4}, {3, 5, 1, 3, 1}};
+        int[][] board = {{0, 0, 0, 0, 0},
+                         {0, 0, 1, 0, 3},
+                         {0, 2, 5, 0, 1},
+                         {4, 2, 4, 4, 2},
+                         {3, 5, 1, 3, 1}};
         int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
         System.out.println(sol.solve(board, moves));
     }
