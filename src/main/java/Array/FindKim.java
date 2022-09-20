aae344d419d5b05d7ba5331f2357cc9f65f2d371
developerOlive <chloehiho@gmail.com>
2022-09-20 21:32:52 +0900

    [문제풀이/Array] FindKim

--- src/main/java/Array/FindKim.java
+++ src/main/java/Array/FindKim.java
@@ -1,2 +1,24 @@
-package Array;public class FindKim {
+package Array;
+
+import java.util.Arrays;
+
+/*
+    https://school.programmers.co.kr/learn/courses/30/lessons/12919
+    (문제)
+    String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아,
+    "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요.
+    seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+ */
+public class FindKim {
+    public String findKim(String[] seoul){
+        //x에 김서방의 위치를 저장하세요.
+        return "김서방은 "+ Arrays.asList(seoul).indexOf("Kim") + "에 있다";
+    }
+
+    // 실행을 위한 테스트코드입니다.
+    public static void main(String[] args) {
+        FindKim kim = new FindKim();
+        String[] names = {"Queen", "Tod","Kim"};
+        System.out.println(kim.findKim(names));
+    }
 }
