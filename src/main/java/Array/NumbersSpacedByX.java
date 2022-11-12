9a0f92c72824bc9cc039bc3a6850dc70da34e04d
developerOlive <chloehiho@gmail.com>
2022-11-12 16:02:52 +0900

    [문제풀이/Array] NumbersSpacedByX - x만큼 간격이 있는 n개의 숫자

---
+++ src/main/java/Array/NumbersSpacedByX.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.Arrays;
+
+public class NumbersSpacedByX {
+
+    /*
+    (문제)
+    https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+    함수 solution은 정수 x와 자연수 n을 입력 받아,
+    x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+    다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+     */
+
+    public static void main(String[] args) {
+        NumbersSpacedByX sol = new NumbersSpacedByX();
+        int x = 2;
+        int n = 5;
+        System.out.println(Arrays.toString(sol.solution(x, n)));
+    }
+
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        int num = x;
+
+        for (int i=0; i< answer.length; i++) {
+            answer[i] = num;
+            num += x;
+        }
+
+        return answer;
+    }
+}
