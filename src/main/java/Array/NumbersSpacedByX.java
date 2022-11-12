5ee398d65f97496dac4ef57124883912dd09e7f2
developerOlive@gmail.com <chloehiho@gmail.com>
2022-11-12 16:14:31 +0900

    [문제풀이/Array] NumbersSpacedByX - x만큼 간격이 있는 n개의 숫자 (2)

--- src/main/java/Array/NumbersSpacedByX.java
+++ src/main/java/Array/NumbersSpacedByX.java
@@ -32,4 +32,16 @@ public class NumbersSpacedByX {
 
         return answer;
     }
+
+    public long[] solution2(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] + x;
+        }
+
+        return answer;
+
+    }
 }
