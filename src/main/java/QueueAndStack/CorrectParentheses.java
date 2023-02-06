106f10621f42e8b518ba2cf6e9c1ac4cc2b6f54d
developerOlive@gmail.com <chloehiho@gmail.com>
2023-02-06 22:43:42 +0900

    [문제풀이/Stack] 올바른 괄호 풀이2

--- src/main/java/QueueAndStack/CorrectParentheses.java
+++ src/main/java/QueueAndStack/CorrectParentheses.java
@@ -1,6 +1,6 @@
 package QueueAndStack;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 public class CorrectParentheses {
 
@@ -17,27 +17,47 @@ public class CorrectParentheses {
 
     public static void main(String[] args) {
         CorrectParentheses sol = new CorrectParentheses();
-        String s = "()()";
-        System.out.println(sol.solution(s));
+        String s = "()())";
+        System.out.println(sol.solution1(s));
     }
 
-    public boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                stack.add(c);
-            } else {
-                if (stack.isEmpty()) {
+    // 올바른 괄호가 아닌 판단 조건
+    // 1. 닫히는 괄호가 들어왔는데 스택이 비어있다.
+    // 2. 마지막에 스택에 데이터가 남아 있다.
+    public boolean solution1(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        char[] a = s.toCharArray();
+        for (char c : a) {
+            if (c == '(') { // 열리는 괄호면 -> 스택에 쌓음
+                stack.push(c);
+            } else { // 닫히는 괄호면
+                if (stack.isEmpty() || stack.pop() == c) { // 처음부터 스택이 비어있는 경우 or 마지막 꺼낸 값이 ')'인 경우
                     return false;
-                } else {
-                    stack.pop();
                 }
             }
         }
-        if (!stack.isEmpty()) {
-            return false;
+
+        return stack.isEmpty(); // 마지막에 데이터가 남아있지 않아야 올바른 괄호임
+    }
+
+    public boolean solution2(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        // O(N)
+        for (int i = 0; i < s.length(); i++) {
+            char now = s.charAt(i); // O(1)
+
+            if (now == '(') {
+                stack.addLast(now); // O(1)
+            } else {
+                if (stack.isEmpty()) { // O(1)
+                    return false;
+                } else {
+                    stack.pollLast(); // O(1)
+                }
+            }
         }
-        return true;
+        return stack.isEmpty(); // O(1)
     }
 }
