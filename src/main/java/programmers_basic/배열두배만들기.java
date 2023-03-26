6313a0987308c4af458c9ca7cdf22cf27922b3c1
developerOlive@gmail.com <chloehiho@gmail.com>
2023-03-26 13:09:53 +0900

    [문제풀이/Basic] 배열 2배 만들기

--- src/main/java/programmers_basic/배열두배만들기.java
+++ src/main/java/programmers_basic/배열두배만들기.java
@@ -1,9 +1,15 @@
-public class Basic {
+package programmers_basic;
+
+import java.util.Arrays;
+
+public class 배열두배만들기 {
 
     /*
     (문제)
+    https://school.programmers.co.kr/learn/courses/30/lessons/120809
 
-
+    정수 배열 numbers가 매개변수로 주어집니다.
+    numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
      */
 
     public static void main(String[] args) {
@@ -11,4 +17,7 @@ public class Basic {
         System.out.println();
     }
 
+    public static int[] solution(int[] numbers) {
+        return Arrays.stream(numbers).map(n -> n * 2).toArray();
+    }
 }
